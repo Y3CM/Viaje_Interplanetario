85338521e41e641a455a03738dfb09ccbf81188e
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-21 22:43:24 -0500

    update menu

--- src/App.java
+++ src/App.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-
+import functions.SelectPlanet;
 public class App {
     public static void main(String[] args) throws Exception {
         var scan = new Scanner(System.in);
-        int opcion = scan.nextInt();
         boolean salir = false;
         do{
             System.out.println("********** Bienvenido a un Viaje Interplanetario **********");
@@ -12,12 +11,13 @@ public class App {
             System.out.println("2. Seleccionar nave espacial");
             System.out.println("3. Iniciar simulación del viaje");
             System.out.println("0. salir del programa");
-
+            
+            int opcion = scan.nextInt();
             switch (opcion) {
                 case 1:
-                    
+                    SelectPlanet.getAllPlanets();
                     break;
-                    
+
                 case 2:
                     
                     break;
@@ -27,7 +27,8 @@ public class App {
                     break;
 
                 case 0:
-                    
+                    salir = true;
+                    System.out.print("Saliendo del programa...");
                     break;
                 
             
---
+++ src/functions/SelectPlanet.java
@@ -0,0 +1,7 @@
+package functions;
+
+public class SelectPlanet {
+    public static void getAllPlanets(){
+        System.out.println("prueba de clase");
+    };
+}
